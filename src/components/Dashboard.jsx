f1776cf89cc8422bd22f77889f31180ed1575a39
Sushank Dhungana <dhunganasushank3@gmail.com>
2025-09-26 10:03:05 +0545

    Updated JSON

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,9 +1,32 @@
 import styles from "./Dashboard.module.css";
+import StudentDetails from "./StudentDetails";
+import studentData from "../assets/StudentData.json";
+
+function getStudentById(id) {
+  const student = JSON.parse(JSON.stringify(studentData));
+  console.log(
+    student["Student Table"].find((s) => s["Student ID"] === id).House
+  );
+  return {
+    student,
+    honors: student["Academic Honors"].find(
+      (honor) => honor["Student ID"] === id
+    ),
+    activities: student["Activities"].find(
+      (activity) => activity["Student ID"] === id
+    ),
+    awards: student["Awards"].find((award) => award["Student ID"] === id),
+    universities: student["Universities"].find(
+      (university) => university["Student ID"] === id
+    ),
+  };
+}
 
 function Dashboard() {
+  // getStudentById("STU001");
   return (
     <div style={{ backgroundColor: "red" }}>
-      <p>this has element</p>
+      <StudentDetails student={getStudentById("STU001")} />
     </div>
   );
 }
---
+++ src/components/StudentDetails.jsx
@@ -0,0 +1,8 @@
+import styles from "./StudentDetails.module.css";
+
+function StudentDetails(student) {
+  console.log(student);
+  return <div></div>;
+}
+
+export default StudentDetails;
